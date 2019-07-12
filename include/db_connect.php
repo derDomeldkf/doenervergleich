e86cf03f34dac0a252ce432180277b970a9e285d
Dominik Eichler <dspam@ldkf.de>
2019-07-12 19:43:45 +0200

    add searchbar

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,1 +1,9 @@
- 
+   
+<?php
+
+	include("config.php");
+	$db = new mysqli($server, $username, $password, $dbname);
+	if ($db->connect_errno) {
+   	echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
+	}
+	$db->query("SET NAMES 'utf8'");
